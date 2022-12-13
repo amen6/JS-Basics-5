96926eae7a726875f31e250a7aa3c8bc23b90941
Amen <jamen.jamne30@gmail.com>
2022-12-13 21:33:49 +0200

    prime numbers

---
+++ script7.js
@@ -0,0 +1,17 @@
+let primeNums = []
+for (let i = 2; i <= 30; i++) {
+    let flag = 0;
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (i > 1 && flag == 0) {
+        primeNums.push(i)
+    }
+}
+
+document.querySelector("body").innerText = primeNums;
