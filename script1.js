621cc0a6110d349c0897c4d8417b3ebe6019083c
Amen <jamen.jamne30@gmail.com>
2022-12-13 16:08:37 +0200

    greetings

---
+++ script1.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn")
+const inputField = document.getElementById("name"); 
+btn.onclick = ()=> {
+    let name = inputField.value;
+    alert("Hello " + name);
+    inputField.value = "";
+}
